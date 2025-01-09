dcdfaeb73f30e240b1e24cd26c5f9893b5c3f22b
vignesh07s <vigneshwarareddys@gmail.com>
2025-01-09 15:55:34 +0530

    updated with backend api

--- frontend/src/components/EventsPage.js
+++ frontend/src/components/EventsPage.js
@@ -10,7 +10,7 @@ function EventsPage() {
     // Fetch all events from the backend
     const fetchEvents = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/v1/events/getEvents');
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/events/getEvents`);
         const data = await response.json();
 
         // Format the date to DD-MM-YYYY
@@ -22,6 +22,7 @@ function EventsPage() {
         setEvents(formattedEvents); // Set the formatted events data
         setLoading(false);
       } catch (err) {
+        console.log(err)
         setError('Error fetching events');
         setLoading(false);
       }
